59dc883c37a8d956b8a93227e0ca4454c0f83c8b
Thainá Matos <thainamatos.if@hotmail.com>
2023-01-17 19:44:06 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { SlArrowRight } from "react-icons/sl";
+import Logo from "./images/logo.svg"
+import MainPhoto from "./images/hero-desktop.jpg";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="body">
+      <div className="container">
+        <header>
+          <img className="logo" src={Logo} />
+
+        </header>
+        
+        <main>
+          <div className="wrapper">
+            <h1>
+              <span className="title">We're</span>
+              <br />
+              coming
+              <br />
+              soon
+            </h1>
+            <p>
+              Hello fellow shoppers! We're currently building our new fashion
+              store. Add your email below to stay up-to-date with announcements
+              and our launch deals.
+            </p>
+            <div className="image">
+              <img src="./image/hero-desktop.jpg" alt="" />
+            </div>
+            <form>
+              <input
+                type="email"
+                className="email"
+                placeholder="Email Adress"
+              />
+              <button className="btn" type="submit">
+              <SlArrowRight size={20} color="#c010b1" />
+              </button>
+        
+            </form>
+          </div>
+          
+        </main>
+        <div>
+          <img src={MainPhoto} alt="" />
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
